fb15684da4e8d74c4680958a6894b2bcc489aa84
Marko Toplak <marko.toplak@gmail.com>
2019-09-24 23:45:08 +0200

    fix travis build

--- scripts/build-widget-catalog.sh
+++ scripts/build-widget-catalog.sh
@@ -4,8 +4,11 @@ set -e
 
 mkdir external
 cd external
-wget -qO- https://github.com/biolab/orange3/archive/master.tar.gz | tar -zxvf -
+wget -qO- https://github.com/biolab/orange3/archive/master.tar.gz | tar -zxf -
 mv orange3-master orange3
 cd ..
 
-python scripts/copy_docs.py external/orange3/doc/
+# Screen must be 24bpp lest pyqt5 crashes, see pytest-dev/pytest-qt/35
+XVFBARGS="-screen 0 1280x1024x24"
+
+catchsegv xvfb-run -a -s "$XVFBARGS" python scripts/copy_docs.py external/orange3/doc/
