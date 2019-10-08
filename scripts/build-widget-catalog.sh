5835ee53a765be0c8ab79704b33a4f304bb7904e
Ajda Pretnar <ajdapretnar@gmail.com>
2019-10-08 13:59:18 +0200

    Add Text add-on to documentation

--- scripts/build-widget-catalog.sh
+++ scripts/build-widget-catalog.sh
@@ -8,6 +8,8 @@ wget -qO- https://github.com/biolab/orange3/archive/master.tar.gz | tar -zxf -
 mv orange3-master orange3
 wget -qO- https://github.com/quasars/orange-spectroscopy/archive/master.tar.gz | tar -zxf -
 mv orange-spectroscopy-master orange-spectroscopy
+wget -qO- https://github.com/biolab/orange3-text/archive/master.tar.gz | tar -zxf -
+mv orange3-text-master orange3-text
 cd ..
 
 # Screen must be 24bpp lest pyqt5 crashes, see pytest-dev/pytest-qt/35
@@ -16,3 +18,4 @@ XVFBARGS="-screen 0 1280x1024x24"
 catchsegv xvfb-run -a -s "$XVFBARGS" python scripts/copy_docs.py \
   external/orange3/doc/ \
   external/orange-spectroscopy/doc/
+  external/oraneg3-text/doc/
